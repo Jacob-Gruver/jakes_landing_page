c5af42e7f8d546895920991d699cf28fec57cba2
Jacob-Gruver <gruvinbro@gmail.com>
2021-05-10 09:59:19 -0600

    init

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // NOTE to host on github publicPath: '/YOUR-GITHUB-REPOSITORY-NAME/',
-  publicPath: '/jakes_landing_page/',
+  publicPath: '/jacob-gruver.github.io/jakes_landing_page/',
   outputDir: 'docs',
   productionSourceMap: false
 }
