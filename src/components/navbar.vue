3c3a80a23b1fe0df814059b1909c861201de6397
Jacob-Gruver <gruvinbro@gmail.com>
2021-05-10 12:09:04 -0600

    init

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,27 +1,31 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
-      <div class="app my-2 d-flex p-2 flex-column align-items-center rounded border border-success">
+      <div
+        class="app my-2 d-flex p-2 flex-column align-items-center rounded border border-success"
+      >
         <span class="font-italic">
-          <h3>
-            Jacob A. Gruver
-          </h3>
+          <h3>Jacob A. Gruver</h3>
         </span>
       </div>
     </router-link>
     <div class="d-flex collapse navbar-collapse" id="navbarText">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item">
-          <router-link :to="{ name: 'About' }" class="nav-link">
+          <!-- <router-link :to="{ name: 'About' }" class="nav-link">
             <i class="app-2 fa fa-user fa-lg" aria-hidden="true"></i>
-          </router-link>
+          </router-link> -->
         </li>
       </ul>
       <span class="navbar-text">
         <div>
-          <a class="linkedin-route btn btn-outline-primary text-primary mr-3" href="https://www.linkedin.com/in/jacob-gruver-6777a61b1/">
+          <a
+            class="linkedin-route btn btn-outline-primary text-primary mr-3"
+            href="https://www.linkedin.com/in/jacob-gruver-6777a61b1/"
+          >
             <h5>
-              Check out my Linkedin Account --> <i class="fa fa-linkedin-square fa-lg"></i>
+              Check out my Linkedin Account -->
+              <i class="fa fa-linkedin-square fa-lg"></i>
             </h5>
           </a>
         </div>
@@ -31,14 +35,12 @@
 </template>
 
 <script>
-
 export default {
-  name: 'Navbar',
+  name: "Navbar",
   data() {
-    return {
-    }
-  }
-}
+    return {};
+  },
+};
 </script>
 
 <style scoped>
@@ -57,22 +59,22 @@ export default {
 a:hover {
   text-decoration: none;
 }
-.nav-link{
+.nav-link {
   text-transform: uppercase;
 }
-.nav-item .nav-link.router-link-exact-active{
+.nav-item .nav-link.router-link-exact-active {
   color: var(--success);
 }
-.linkedin-route:hover{
+.linkedin-route:hover {
   transition: transform 1.5s;
   transform: scale(1.05);
 }
-.app:hover{
-    transition: transform 1.5s;
-    transform: scale(1.1);
-  }
-  .app-2:hover{
-    transition: transform 1.5s;
-    transform: scale(1.5);
-  }
+.app:hover {
+  transition: transform 1.5s;
+  transform: scale(1.1);
+}
+.app-2:hover {
+  transition: transform 1.5s;
+  transform: scale(1.5);
+}
 </style>
--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="about flex-grow-1 d-flex flex-column align-items-center justify-content-center">
+  <div
+    class="about flex-grow-1 d-flex flex-column align-items-center justify-content-center"
+  >
     <!-- <h1>This is the about page</h1> -->
-    <img class="" src="../assets/img/Gruver-Jacob.png">
+    <!-- <img class="" src="../assets/img/Gruver-Jacob.png"> -->
   </div>
 </template>
 
 <script>
 export default {
-  name: 'AboutPage'
-}
+  name: "AboutPage",
+};
 </script>
 <style scoped lang="scss">
-.about{
-  animation:  fadeInAnimation ease 3s;
+.about {
+  animation: fadeInAnimation ease 3s;
   animation-iteration-count: 1;
   animation-fill-mode: forwards;
   @keyframes fadeInAnimation {
@@ -22,11 +24,10 @@ export default {
     100% {
       opacity: 1;
     }
+  }
 }
-}
-img{
+img {
   height: 1800px;
   width: 1400px;
 }
-
 </style>
